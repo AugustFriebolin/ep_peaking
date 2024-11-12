7170686a73705f7117bab1202697d4aa60b4e02e
Axel Schmidt <axelschmidt@gwu.edu>
2024-11-12 12:39:53 -0500

    Added a program to convert output files to text files in lund format.

--- generator.cpp
+++ generator.cpp
@@ -9,14 +9,10 @@
 #include <iostream>
 #include <unistd.h>
 
+#include "ep_constants.h"
+
 using namespace std;
 
-const double mP=0.938272;
-const double me=0.000511;
-const double muP=2.79;
-const double alpha = 0.0072973525;
-const double hbarc=0.1973; // GeV*fm
-const double nbGeVSq = hbarc*hbarc *1.E7;
 
 double Gdip(double QSq)
 {
--- generator_born.cpp
+++ generator_born.cpp
@@ -9,14 +9,9 @@
 #include <iostream>
 #include <unistd.h>
 
-using namespace std;
+#include "ep_constants.h"
 
-const double mP=0.938272;
-const double me=0.000511;
-const double muP=2.79;
-const double alpha = 0.0072973525;
-const double hbarc=0.1973; // GeV*fm
-const double nbGeVSq = hbarc*hbarc *1.E7;
+using namespace std;
 
 double Gdip(double QSq)
 {
